0a99ae302e704aaf167ccaddd3cfe53ef5263fcc
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-09-22 18:56:17 -0300

    chore: adding fectures on home components

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,50 +1,58 @@
-import * as S from "./style";
-import { ReactComponent as Search } from "assets/icons/search.svg";
 import Menu from "components/Menu";
+import { ReactComponent as Search } from "assets/icons/search.svg";
+import { DateTime } from "luxon";
+import * as S from "./style";
 import { RoutePath } from "types/routes";
 import { navigationItems } from "data/navigation";
-import { DateTime } from "luxon";
 import ProductItemList from "components/ProductItemList";
 import ProductItem from "components/ProductItem";
 import OrderDetails from "components/OrderDetails";
+import Overlay from "components/Overlay";
+import CheckoutSection from "components/CheckoutSection";
 
 const Home = () => {
-	const dateDescription = DateTime.now().toLocaleString({
-		...DateTime.DATE_SHORT,
-		weekday: "long",
-	  });
-	return <S.Home>
-		<Menu 
-		active={RoutePath.HOME}
-		navItems = {navigationItems}/>
-		<S.HomeContent>
-	<header>
-	<S.HomeHeaderDetails>
-		<div>
-			<S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
-			<S.HomeHeaderDetailsDate>{dateDescription}</S.HomeHeaderDetailsDate>
-		</div>
-        <S.HomeHeaderDetailsSearch>
-		<Search />
-	<input type="text" placeholder="Procure pelo sabor" />
-		</S.HomeHeaderDetailsSearch>
-	</S.HomeHeaderDetails>
-</header>
-<div>
-		<S.HomeProductTitle>
-			<b>Pizzas</b>
-		</S.HomeProductTitle>
-		<S.HomeProductList>
-      		<ProductItemList>
-			  <ProductItem />
-			</ProductItemList>
-		</S.HomeProductList>
-	</div>
-</S.HomeContent>
-<aside>
-<OrderDetails />
-	</aside>
-</S.Home>
+  const dateDescription = DateTime.now().toLocaleString({
+    ...DateTime.DATE_SHORT,
+    weekday: "long",
+  });
+
+  return (
+    <S.Home>
+      <Menu active={RoutePath.HOME} navItems={navigationItems} />
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                {dateDescription}
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Procure pelo sabor" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Pizzas</b>
+          </S.HomeProductTitle>
+          <S.HomeProductList>
+            <ProductItemList>
+              <ProductItem />
+            </ProductItemList>
+          </S.HomeProductList>
+        </div>
+      </S.HomeContent>
+      <aside>
+        <OrderDetails />
+      </aside>
+      <Overlay>
+        <CheckoutSection />
+      </Overlay>
+    </S.Home>
+  );
 };
 
 export default Home;
